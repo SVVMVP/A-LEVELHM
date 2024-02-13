ffd23e3821ef61aee8cf1dde29fcd9d2f07466b7
SVVMVP <sheveryuk01@gmail.com>
2024-02-13 21:21:16 +0200

    add HM

---
+++ src/main/java/HM4/Task2.java
@@ -0,0 +1,46 @@
+package HM4;
+
+import java.util.Random;
+
+public class Task2 { public static class Main {
+    public static void main(String[] args) {
+        // Задаємо розмір масиву
+        int arraySize = 1000;
+
+        // Заповнюємо масив випадковими цілими числами
+        int[] array = new int[arraySize];
+        Random random = new Random();
+        for (int i = 0; i < arraySize; i++) {
+            array[i] = random.nextInt(100) + 1; // Ви можете змінити діапазон відповідно до вашого завдання
+        }
+
+        // Знаходимо кількість простих чисел у масиві
+        int primeCount = countPrimes(array);
+        System.out.println("Кількість простих чисел в масиві: " + primeCount);
+    }
+
+    // Метод для перевірки, чи є число простим
+    private static boolean isPrime(int number) {
+        if (number < 2) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // Метод для підрахунку кількості простих чисел у масиві
+    private static int countPrimes(int[] array) {
+        int count = 0;
+        for (int value : array) {
+            if (isPrime(value)) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
+}
---
+++ src/main/java/HM5/Task1.java
@@ -0,0 +1,37 @@
+package HM5;
+
+public class Task1 { public static void main(String[] args) {
+    // Задаємо розмір двовимірного масиву
+    int rows = 4;
+    int cols = 3;
+
+    // Створюємо двовимірний масив та заповнюємо його
+    int[][] array = new int[rows][cols];
+    fillArray(array);
+
+    // Виводимо отриманий масив
+    printArray(array);
+}
+
+    // Метод для заповнення двовимірного масиву вказаним чином
+    private static void fillArray(int[][] array) {
+        int counter = 1;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                array[i][j] = (i % 2 == 0) ? counter : -counter;
+                counter++;
+            }
+        }
+    }
+
+    // Метод для виведення двовимірного масиву
+    private static void printArray(int[][] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+}
