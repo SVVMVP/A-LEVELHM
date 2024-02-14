67ec29ba2ed53cdd979819cc90676cb76c9dd6fb
SVVMVP <sheveryuk01@gmail.com>
2024-02-14 18:49:01 +0200

    add HW

---
+++ src/main/java/HW10/Task1.java
@@ -0,0 +1,43 @@
+package HW10;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Task1 {
+    public static void main(String[] args) {
+        // Створюємо ArrayList та додаємо елементи
+        ArrayList<String> list = new ArrayList<>();
+        list.add("One");
+        list.add("Two");
+        list.add("Three");
+        list.add("Four");
+        list.add("Five");
+
+        // Обхід ArrayList за допомогою циклу for
+        System.out.println("Обхід ArrayList за допомогою циклу for:");
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+
+        // Обхід ArrayList за допомогою циклу for-each
+        System.out.println("\nОбхід ArrayList за допомогою циклу for-each:");
+        for (String element : list) {
+            System.out.println(element);
+        }
+
+        // Обхід ArrayList за допомогою циклу while
+        System.out.println("\nОбхід ArrayList за допомогою циклу while:");
+        int index = 0;
+        while (index < list.size()) {
+            System.out.println(list.get(index));
+            index++;
+        }
+
+        // Обхід ArrayList за допомогою ітератора
+        System.out.println("\nОбхід ArrayList за допомогою ітератора:");
+        Iterator<String> iterator = list.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+    }
+}
---
+++ src/main/java/HW8/Task2.java
@@ -0,0 +1,24 @@
+package HW8;
+
+public class Task2 {public static void main(String[] args) {
+    // Реалізація функціонального інтерфейсу через лямбда-вираз
+    Pow pow = (number, power) -> {
+        int result = 1;
+        for (int i = 0; i < power; i++) {
+            result *= number;
+        }
+        return result;
+    };
+
+    // Приклад використання функціонального інтерфейсу
+    int number = 2;
+    int power = 3;
+    int result = pow.pow(number, power);
+    System.out.println(number + " в степені " + power + " = " + result);
+}
+}
+
+// Функціональний інтерфейс для піднесення числа до степені
+interface Pow {
+    int pow(int number, int pow);
+}
