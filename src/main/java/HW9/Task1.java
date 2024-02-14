57d00460409be11699c5d283d33f661e58c99b09
SVVMVP <sheveryuk01@gmail.com>
2024-02-14 18:58:55 +0200

    add HW

---
+++ src/main/java/HW9/Task1.java
@@ -0,0 +1,13 @@
+package HW9;
+
+public class Task1 {
+    public static void main(String[] args) {
+        try {
+            throw new Exception("This is an example of throwing an exception!");
+        } catch (Exception e) {
+            System.out.println("Caught exception: " + e.getMessage());
+        } finally {
+            System.out.println("Finally block: Program execution completed");
+        }
+    }
+}
