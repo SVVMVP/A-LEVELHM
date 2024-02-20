c375b290e18b7ccf9ab0dc87c005d301e83a200a
SVVMVP <sheveryuk01@gmail.com>
2024-02-20 16:29:04 +0200

    add HW

--- src/main/java/ua/sheveriuk/hw4/Task3.java
+++ src/main/java/ua/sheveriuk/hw4/Task3.java
@@ -1,5 +1,6 @@
 package ua.sheveriuk.hw4;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class Task3 {
@@ -7,12 +8,15 @@ public class Task3 {
         int arraySize = 2000;
         int[] array = generateRandomArray(arraySize);
 
-        replaceEvenWithZero(array);
+        int[] modifiedArray = replaceEvenWithZero(array);
 
-        printArray("Масив після заміни четних значень на нулі:", array);
+        printArray("Масив після заміни четних значень на нулі:", modifiedArray);
+
+        // Перевірка, що оригінальний масив не змінений
+        printArray("Оригінальний масив:", array);
     }
 
-    // Метод для генерации массива случайных чисел
+    // Метод для генерації масиву випадкових чисел
     private static int[] generateRandomArray(int size) {
         int[] array = new int[size];
         Random random = new Random();
@@ -22,20 +26,23 @@ public class Task3 {
         return array;
     }
 
-    // Метод для замены четных значений в массиве на нули
-    private static void replaceEvenWithZero(int[] array) {
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] % 2 == 0) {
-                array[i] = 0;
+    // Метод для заміни парних значень в масиві на нулі і повернення зміненої копії масиву
+    private static int[] replaceEvenWithZero(int[] array) {
+        int[] newArray = Arrays.copyOf(array, array.length); // Копіюємо масив
+        for (int i = 0; i < newArray.length; i++) {
+            if (newArray[i] % 2 == 0) {
+                newArray[i] = 0; // Замінюємо парні значення на нулі
             }
         }
+        return newArray; // Повертаємо змінену копію масиву
     }
 
-    // Метод для вывода массива на экран
+    // Метод для виводу масиву на екран
     private static void printArray(String message, int[] array) {
         System.out.println(message);
         for (int value : array) {
             System.out.print(value + " ");
         }
+        System.out.println(); // Після виводу кожного масиву додамо порожній рядок для зручності читання
     }
 }
